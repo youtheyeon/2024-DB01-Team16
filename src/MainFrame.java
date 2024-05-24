8c1b1c869412efdc03ae675ffdc190e619aea929
hunheay123 <bihunheay1@gmail.com>
2024-05-24 22:48:07 +0900

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -8,8 +8,11 @@ public class MainFrame extends JFrame {
     private LibraryManager libraryManager;
     private JButton borrowButton;
 
-    public MainFrame(UserSession session) {
+    public MainFrame(UserSession session, Connection conn) { // Connection conn 추가
+        
         this.userSession = session;
+        this.libraryManager = new LibraryManager(conn); //db 연동 전달
+        
         this.libraryManager = new LibraryManager();
         setTitle("Library Management System");
         setSize(800, 600);
