5d02ed74199e586148eb5889552965874e3090a9
youtheyeon <yuseoyeon@ewhain.net>
2024-05-25 22:43:42 +0900

    fix : SQL 파일 수정

--- sql files/dropschema.sql
+++ sql files/dropschema.sql
@@ -1,15 +1,18 @@
--- BookDetails 뷰 삭제
 DROP VIEW IF EXISTS BookDetails;
 DROP VIEW IF EXISTS FilteredBooks;
 
--- 인덱스 삭제
-DROP INDEX IF EXISTS idx_title ON Book;
-DROP INDEX IF EXISTS idx_user_name ON User;
-DROP INDEX IF EXISTS idx_borrow_user ON Borrow;
-DROP INDEX IF EXISTS idx_borrow_book ON Borrow;
-DROP INDEX IF EXISTS idx_category ON Category;
+DROP INDEX idx_title ON Book;
+DROP INDEX idx_user_name ON User;
+DROP INDEX idx_category ON Category;
 
--- 테이블 삭제
+-- First drop the foreign key constraints
+ALTER TABLE Borrow DROP FOREIGN KEY borrow_ibfk_1;
+ALTER TABLE Borrow DROP FOREIGN KEY borrow_ibfk_2;
+-- Drop the indexes
+DROP INDEX idx_borrow_user ON Borrow;
+DROP INDEX idx_borrow_book ON Borrow;
+
+-- Drop the tables
 DROP TABLE IF EXISTS Borrow;
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Book;
