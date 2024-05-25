eb7c428235f4ccbb14fa1456d5324178e19e75f6
youtheyeon <yuseoyeon@ewhain.net>
2024-05-25 21:20:36 +0900

    chore : 비밀번호 삭제

--- java files/src/App.java
+++ java files/src/App.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class App {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/db0116";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "seoyeon02!";
+    private static final String JDBC_PASSWORD = "";
     private static Connection connection;
 
     public static void main(String[] args) {
